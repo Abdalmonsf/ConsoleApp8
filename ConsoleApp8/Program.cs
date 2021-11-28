dfdd853ecc49b6d305d20ebc3d46bac541146e97
Nvidia <Nvidia@LAPTOP-DH5UU1QR>
2021-11-28 20:01:43 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList<string> list1 = new LinkedList<string>();
+
+            list1.AddFirst("yes");
+            list1.AddLast("nO");
+            LinkedListNode<String> nozzle = new LinkedListNode<string>("Done");
+            list1.AddFirst(nozzle);
+            list1.AddBefore(nozzle, "Tool");
+            list1.AddAfter(nozzle, "Cool");
+
+            Console.WriteLine("The LinkedList Before Reversing: ");
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("The LinkedList After Reversing: ");
+
+            foreach (var item in list1.Reverse())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
